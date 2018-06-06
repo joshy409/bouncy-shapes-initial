996d281f19fa107035f94b588f8f3159d2f664b1
Joshua Yang <ybd1992@hotmail.com>
2018-06-05 21:53:33 -0700

    added EllipseRenderer

--- BouncyShapes/EllipseRenderer.cpp
+++ BouncyShapes/EllipseRenderer.cpp
@@ -9,6 +9,6 @@ void EllipseRenderer::Render(ID2D1HwndRenderTarget & target, const MPoint2F & po
 	auto color = D2D1_COLOR_F{ _color.r, _color.g, _color.b, _color.a };
 	_brushPtr->GetBrush()->SetColor(color);
 	auto center = D2D1::Point2F(position.x, position.y);
-	auto ellipse = D2D1::Ellipse(center, _radius, _radius);
+	auto ellipse = D2D1::Ellipse(center, _radius, _radius*7);
 	target.FillEllipse(ellipse, _brushPtr->GetBrush().Get());
 }
