b3bf16b8940b880bc649b5ed2e5bb0e3ffcbb5ba
Joshua Yang <ybd1992@hotmail.com>
2018-06-05 21:53:06 -0700

    added EllipseRenderer

---
+++ BouncyShapes/EllipseRenderer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "IRenderer.h"
+#include <memory>
+#include "BrushDeviceResource.h"
+
+class EllipseRenderer : public IRenderer {
+public:
+	EllipseRenderer(std::shared_ptr<BrushDeviceResource> brush, Color4F color, float radius);
+
+	void Render(ID2D1HwndRenderTarget& target, const MPoint2F& position) const override;
+
+private:
+	std::shared_ptr<BrushDeviceResource> _brushPtr;
+	Color4F _color;
+	float _radius;
+
+};
